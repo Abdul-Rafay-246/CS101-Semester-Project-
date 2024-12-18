09b98d93bf5e91b430e48bf4dc1416c6cf77e953
Abdul-Rafay-246 <abdulrafayraees5@gmail.com>
2024-12-19 02:34:51 +0500

    tested the first draft and wrote the comments to make the code better

--- ideatesting.cpp
+++ ideatesting.cpp
@@ -116,3 +116,7 @@ int main() {
 
     return 0;
 }
+//are you confortable wuth programming concept question makes no sense
+//file handling is ok but the result is not reliable
+//should try to do the code without vectors
+//should give the user options like 1/2/3 in every questions instead of asking them to type
